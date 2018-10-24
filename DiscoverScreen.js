7062f2cb7da853e9e81d7539cd88278707892c82
Ronnel Davis <ronnedavis1996@gmail.com>
2018-10-24 12:14:46 +0400

    Saving to localstorage

--- DiscoverScreen.js
+++ DiscoverScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SwipeCards from 'react-native-swipe-cards'
 import items from './data.js'
-import { Dimensions } from "react-native";
+import { Dimensions, AsyncStorage } from "react-native";
 
 var width = Dimensions.get("window").width;
 var height = Dimensions.get("window").height;
@@ -10,9 +10,6 @@ var height = Dimensions.get("window").height;
 class Card extends React.Component {
   constructor(props) {
     super(props);
-  }
-
-  componentDidMount() {
   }
 
   render() {
@@ -44,19 +41,62 @@ export default class DiscoverScreen extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+     arr: [],
       cards: items.map(a => {
         return {"text": a}
       })
     };
   }
 
+
+  componentDidMount() {
+    this.getArray();
+  }
+
+  getArray = () => {
+    AsyncStorage.getItem('data').then(req => JSON.parse(req)).then(json => {
+        console.log("Found");
+        console.log(json || []);
+        this.setState({arr: json || []});
+    }).catch(e => {
+        console.log("Not found");
+        this.setState({arr: []});
+        this.setArray();
+    })
+  }
+
+  setArray = () => {
+    AsyncStorage.setItem('data', JSON.stringify(this.state.arr))
+  }
+
+  handleYup = (card) => {
+    console.log("YUP");
+    console.log(card.text);
+    var a = this.state.arr;
+    a.push({title: card.text, completed: false});
+    this.setState({arr: a});
+    this.setArray();
+  }
+
+  handleNope = (card) => {
+    console.log("NOPE");
+    console.log(card.text);
+  }
+  
+  handleMaybe = (card) => {
+    console.log("MAYBE");
+    console.log(card.text);
+    var a = this.state.arr;
+    a.push({title: card.text, completed: true});
+    this.setState({arr: a});
+    this.setArray();
+  }
+
   render() {
     return (
         <View style={styles.container}>
+        <Text style={styles.header}>Discover</Text>
       <SwipeCards
-      containerStyle={styles.wrapper}
-        stackOffsetY={10}
-        stackOffsetX = {0}
         cards={this.state.cards}
         renderCard={(cardData) => <Card {...cardData} />}
         renderNoMoreCards={() => <NoMoreCards />}
@@ -82,11 +122,13 @@ export default class DiscoverScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    wrapper: {
-        height: height,
-        width: width,
-        backgroundColor: "red"
-    },
+    header: {
+        fontWeight: "900",
+        fontFamily: "Avenir",
+        fontSize: 44,
+        marginLeft: 20,
+        marginTop: 44
+    },  
     container: {
         flex: 1,
         backgroundColor: "#eee",
@@ -98,6 +140,7 @@ const styles = StyleSheet.create({
     width: 300,
     height: 300,
     padding: 20,
+    marginBottom: 100,
     elevation: 3,
     borderRadius: 15,
     backgroundColor: "#fff",
